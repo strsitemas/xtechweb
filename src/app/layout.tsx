889a3b97cfe062f8e7500029ed02895965113f47
Edson Junior dos Santos <salvacao.cristo@gmail.com>
2026-09-17 06:55:58 -0300

    feat: adiciona Google Analytics ao XTechWeb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+      <GoogleAnalytics gaId="G-HZX91WJG4C" />
     </html>
   );
 }
